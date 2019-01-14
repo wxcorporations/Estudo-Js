0348cf446d0b866042669645642d90ba06e885d8
Fernando J <fernando.barros@3mw.com.br>
2019-01-14 19:53:49 -0200

    Estudo Padrao module Js

---
+++ modulo-06/padrao_module.js
@@ -0,0 +1,56 @@
+let Gastos_controller = ( function () {
+    let Gastos = function () {
+        let total_gasto = 0 ;
+        let self = this ;
+
+        this.lista_gastos = [];
+        this.novo_gasto = function ( descricao , valor , tag ) {
+            let gasto = {
+                descricao : descricao ,
+                valor : valor ,
+                tag : tag
+            }
+
+            self.lista_gastos.push( gasto ) ;
+        };
+        this.atualizar_total_gastos = function ( value ) {
+            total_gasto = value ;
+        };
+
+        this.get_totalGasto = function () {
+            let valor = total_gasto ;
+            return  valor ;
+        }
+
+    };
+
+    Gastos.prototype.somar_gasto = function () {
+        let soma = 0 ;
+        this.lista_gastos.forEach( function (item , index)
+            {
+                soma += item['valor'] ;
+            }
+        )
+        this.atualizar_total_gastos( soma ) ;
+    }
+
+
+    return {
+        Gastos : function () {
+            return new Gastos();
+        }
+    };
+})();
+
+
+let newGasto = Gastos_controller.Gastos();
+
+newGasto.novo_gasto('carro', 200 , 'transporte');
+newGasto.novo_gasto('uber 10/05 ', 25 , 'transporte');
+newGasto.novo_gasto('uber 10/05 ', 25 , 'transporte');
+newGasto.novo_gasto('uber 10/05 ', 25 , 'transporte');
+newGasto.novo_gasto('uber 10/05 ', 25 , 'transporte');
+newGasto.somar_gasto();
+
+
+console.log(newGasto.get_totalGasto());
