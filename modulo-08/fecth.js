7febdf3cafa189880bbbf0aecaf0ef9bde44a7c3
Fernando J <fernando.barros@3mw.com.br>
2019-01-18 17:54:09 -0200

    add arquivos com exemplo do metodo fecth

---
+++ modulo-08/fecth.js
@@ -0,0 +1,13 @@
+function getWeather (woeid) {
+    fetch('https://crossorigin.me/htops://www.metaweather.com/api/location/' + woeid + '/')
+        .then(result => {
+            console.log(result);
+            return result.json() ;
+        })
+        .then( data =>{
+            console.log(data)
+        })
+
+}
+
+getWeather(44418);
